44df9584dd23fdd370f43dffea0c4b35d2bcc641
Mohamed Abdallah <mo7amed.3bdalla7@gmail.com>
2020-05-27 17:02:22 +0400

    Extract remote urls from a git repo

--- grurl.go
+++ grurl.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	fmt.Print("Hello From Grurl")
+	config := RemoteConfig{data: map[string]string{}}
+
+	config.ParseFile("./.git/config")
+
+	fmt.Print(config.data)
+}
+
+type RemoteConfig struct {
+	configPath string
+	data       map[string]string
+}
+
+func (c *RemoteConfig) ParseFile(path string) error {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	c.configPath = path
+
+	c.Parse(file)
+
+	return nil
+}
+
+func (c *RemoteConfig) Parse(file *os.File) {
+	const remoteRowStart = `[remote "`
+	const remoteUrlRowStart = `url = `
+	var remoteName string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if strings.Index(line, remoteRowStart) != -1 {
+			remoteName = line[len(remoteRowStart) : len(line)-2]
+
+			c.data[remoteName] = ""
+		}
+
+		if strings.Index(line, remoteUrlRowStart) != -1 && remoteName != "" {
+			url := line[len(remoteUrlRowStart):]
+			url = SshUrlToHttpUrl(url)
+
+			c.data[remoteName] = url
+			remoteName = ""
+		}
+	}
+}
+
+//Checks if a url is a valid ssh url and converts it to http url or returns the same url
+func SshUrlToHttpUrl(url string) string {
+	regex := regexp.MustCompile(
+		`^(?P<user>.+)@(?P<host>.+?):(?P<port>[0-9]*)(?P<path>.+)$`,
+	)
+
+	groups := regex.FindStringSubmatch(url)
+
+	if len(groups) == 5 {
+		url = ""
+
+		url = fmt.Sprintf("https://%s", groups[2])
+
+		if groups[3] != "" {
+			url += fmt.Sprintf(":%s", groups[3])
+		}
+
+		if strings.Index(groups[4], "/") == 0 {
+			url += fmt.Sprintf("%s", groups[4])
+		} else {
+			url += fmt.Sprintf("/%s", groups[4])
+		}
+
+		return url
+	}
+
+	return url
 }
--- grurl_test.go
+++ grurl_test.go
@@ -1,7 +1,70 @@
 package main
 
-func ExampleMain() {
-	main()
-	// Output:
-	// Hello From Grurl
+import (
+	"os"
+	"testing"
+)
+
+const ConfigPath  ="./.git/config"
+const InvalidConfigPath  ="./.git/invalid_config"
+const SshUrl  ="git@github.com:mo7amed-3bdalla7/grurl.git"
+const HttpUrl  ="https://github.com/mo7amed-3bdalla7/grurl.git"
+
+func NewConfig() *RemoteConfig {
+	return &RemoteConfig{data: map[string]string{}}
+}
+
+func TestRemoteConfig_ParseFile_validConfigPath(t *testing.T) {
+	config := NewConfig()
+
+	config.ParseFile(ConfigPath)
+
+	if len(config.data) == 0 {
+		t.Error("Empty git config file")
+	}
+}
+
+func TestRemoteConfig_ParseFile_invalidConfigPath(t *testing.T) {
+	config := NewConfig()
+
+	err := config.ParseFile(InvalidConfigPath)
+
+	if err == nil {
+		t.Error("Valid git config path")
+	}
+}
+
+func TestRemoteConfig_Parse(t *testing.T) {
+
+	file, err := os.Open(ConfigPath)
+
+	if err != nil {
+		t.Error("Wrong git config path")
+	}
+
+	defer file.Close()
+
+	config := NewConfig()
+
+	config.configPath = ConfigPath
+
+	config.Parse(file)
+
+	if ConfigPath !=config.configPath {
+		t.Error("Invalid config file path storing")
+	}
+
+	if len(config.data) == 0 {
+		t.Error("Empty git config file")
+	}
+
+}
+
+func TestSshUrlToHttpUrl(t *testing.T) {
+
+	url:=SshUrlToHttpUrl(SshUrl)
+
+	if url !=HttpUrl {
+		t.Error("Invalid ssh url")
+	}
 }
